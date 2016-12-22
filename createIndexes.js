5fab7f0782f45e6f60fded27f5ec6ed5b37b998e
Mark Essel <messel@gmail.com>
2016-12-22 11:25:19 -0500

    much faster queries

--- createIndexes.js
+++ createIndexes.js
@@ -5,20 +5,40 @@ r.connect( {host: 'localhost', port: 28015, db: 'test'}, (err, conn) => {
 
   const sTable = 'incidents';
 
-  r.table(sTable).indexCreate('ts').run(conn).then( (result) => {
-    console.log({ action: 'indexCreate ts', result: JSON.stringify(result, null, 2) });
-  })
-  r.table(sTable).indexCreate('latitude').run(conn).then( (result) => {
-    console.log({ action: 'indexCreate latitude', result: JSON.stringify(result, null, 2) });
-  })
-  r.table(sTable).indexCreate('longitude').run(conn).then( (result) => {
-    console.log({ action: 'indexCreate longitude', result: JSON.stringify(result, null, 2) });
-  })
-  .then( () => {
-    return r.table(sTable).indexCreate('ll', {multi: true}).run(conn);
+  r.table(sTable).indexCreate('ts').run(conn)
+  .catch( (err) => {
+    console.log({ action: 'indexCreate ts err', err:err });
   })
   .then( (result) => {
-    console.log({ action: 'indexCreate ll', result: JSON.stringify(result, null, 2) });
+    console.log({ action: 'indexCreate ts', result: JSON.stringify(result, null, 2) });
+    return r.table(sTable).indexCreate('latitude').run(conn);
+  })
+  .catch( (err) => {
+    console.log({ action: 'indexCreate latitude err', err:err });
+  })
+  .then( (result) => {
+    console.log({ action: 'indexCreate latitude', result: JSON.stringify(result, null, 2) });
+    return r.table(sTable).indexCreate('longitude').run(conn);
+  })
+  .catch( (err) => {
+    console.log({ action: 'indexCreate longitude err', err:err });
+  })  
+  .then( (result) => {
+    console.log({ action: 'indexCreate longitude', result: JSON.stringify(result, null, 2) });
+    return r.table(sTable).indexCreate('latitude_ts, lambda x: [x["latitude"], x["ts"]]').run(conn);
+  })
+  .catch( (err) => {
+    console.log({ action: 'indexCreate latitude_ts err', err:err });
+  })  
+  .then( (result) => {
+    console.log({ action: 'indexCreate latitude ts', result: JSON.stringify(result, null, 2) });
+    return r.table(sTable).indexCreate('longitude_ts, lambda x: [x["longitude"], x["ts"]]').run(conn);
+  })
+  .catch( (err) => {
+    console.log({ action: 'indexCreate longitude_ts err', err:err });
+  })  
+  .then( (result) => {
+    console.log({ action: 'indexCreate longitude ts', result: JSON.stringify(result, null, 2) });
     process.exit(0);
   })
   .catch( (err) => {
---
+++ queryFastTable.js
@@ -0,0 +1,75 @@
+const r = require('rethinkdbdash')({host: 'localhost', port: 28015, db: 'test' });
+
+
+const sTable = 'incidents';
+
+
+const center    = [-73.993549, 40.727248];
+const lowerLeft = [-74.009180, 40.716425];
+const deltaLon  = 2 * Math.abs(lowerLeft[0] - (-73.97725));
+const deltaLat  = 2 * Math.abs(lowerLeft[1] - (40.7518692));
+
+const NQueries = 10000;
+const N = 20;
+
+
+let t0 = Date.now();
+
+let aPromises = [];
+for (let i=0;i < NQueries;i++) {
+  const searchLon       = lowerLeft[0] + Math.random() * deltaLon;
+  const searchLat       = lowerLeft[1] + Math.random() * deltaLat;
+  const halfWinLon      = Math.random() * 0.04;
+  const halfWinLat      = Math.random() * 0.04;
+
+  const lowerLatitude   = searchLat - halfWinLat;
+  const lowerLongitude  = searchLon - halfWinLon;
+  const upperLatitude   = searchLat + halfWinLat;
+  const upperLongitude  = searchLon + halfWinLon;
+
+
+  let aQuery = r.table(sTable)
+  .orderBy({ index: r.desc('ts')})
+  // .filter(function (oIncident) {
+  //   return oIncident('latitude').gt(lowerLatitude)
+  //     .and(oIncident('latitude').lt(upperLatitude))
+  //     .and(oIncident('longitude').gt(lowerLongitude))
+  //     .and(oIncident('longitude').lt(upperLongitude))
+  // })
+  .between(['latitude', lowerLatitude ], ['latitude',  upperLatitude ])
+  // .between(['longitude',lowerLongitude], ['longitude', upperLongitude])
+  // .filter(function (oIncident) {
+  //   return oIncident('latitude').gt(lowerLatitude);
+  // })
+  // .filter(function (oIncident) {
+  //   return oIncident('latitude').lt(upperLatitude);
+  // })
+  .filter(function (oIncident) {
+    return oIncident('longitude').gt(lowerLongitude);
+  })
+  .filter(function (oIncident) {
+    return oIncident('longitude').lt(upperLongitude);
+  })
+  // .orderBy({ index: r.desc('ts')})
+  .limit(N)
+  .then( (results) => {
+    // console.log({ action: 'query', aKeys: aKeys, ll: [lowerLatitude,upperLatitude,lowerLongitude,upperLongitude] });
+    return results
+  })
+
+  aPromises.push(aQuery);
+}
+
+
+Promise.all(aPromises).then( (aResults) => {
+  let t1 = Date.now();
+  console.log({ queriesTimeMS: t1-t0, queriesPerSecond: NQueries / ( (t1-t0)/1000 ) })
+  for (let i in aResults) {
+    const result = aResults[i];
+    const aKeys = result.map( (oMatch) => oMatch.id )
+    // console.log({ action: 'query', aKeys: aKeys });
+  }
+})
+.catch( (err) => {
+  throw err;
+})       
